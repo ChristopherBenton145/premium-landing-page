36a605db68108e6796ed2cd50d5c4b95074eea0b
TempUsername2436 <c.benton.145@gmail.com>
2022-04-04 13:40:28 -0700

    Added the services section

--- src/components/text-feature.js
+++ src/components/text-feature.js
@@ -9,7 +9,28 @@ export default function TextFeature({
   btnURL = '#',
 }) {
   return (
-    <h1>TextFeature</h1>
+    <Box sx={styles.card}>
+      <Box sx={styles.wrapper}>
+        <Text as="p" sx={styles.wrapper.subTitle}>{subTitle}</Text>
+        <Heading as="h2" sx={styles.wrapper.title}>
+          {title}
+        </Heading>
+      </Box>
+
+      {description && (
+        <Text as="p" className="description" sw={styles.description}>
+          {description}
+        </Text>
+      )}
+
+      {btnName && (
+        <Link href={btnURL} variant="default">
+          <Button variant="praimary" aria-label={btnName}>
+            {btnName}
+          </Button>
+        </Link>
+      )}
+    </Box>
   );
 }
 
